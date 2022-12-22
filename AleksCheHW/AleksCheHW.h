450f482996f71794d81742a55ea4eda38f3fa5dd
Jack1eAW <alcheb2014@gmail.com>
2022-12-22 21:34:40 +0300

    Initial Commit

---
+++ AleksCheHW/AleksCheHW.h
@@ -0,0 +1,18 @@
+//
+//  AleksCheHW.h
+//  AleksCheHW
+//
+//  Created by Aleksandr Chebotarev on 12/22/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AleksCheHW.
+FOUNDATION_EXPORT double AleksCheHWVersionNumber;
+
+//! Project version string for AleksCheHW.
+FOUNDATION_EXPORT const unsigned char AleksCheHWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AleksCheHW/PublicHeader.h>
+
+
